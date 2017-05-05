c4d58aaa3f999e3b1db27b55bdb17115fb935153
Akuli <akuviljanen17@gmail.com>
2017-05-05 11:28:55 +0300

    more lulz

--- hello.py
+++ hello.py
@@ -3,9 +3,9 @@
 
 from posix import *
 from io import *
+from itertools import *
 from socket import *
 
-dict.update(globals(), zip(map(chr, range(97,101)),
-pipe()+socketpair())); (fork() and d.send(b'Hello World!\n') or fork()
-and print(next(open(++a,'r'))) or BufferedWriter.write(open(--b,'wb'),
-c.recv(1<<10)))
+dict.update(globals(),zip(map(chr,count(97)),pipe()+socketpair()))
+fork()and d.send(b'Hello World!\n')or fork()and print(next(open(++a,
+'r')))or BufferedWriter.write(open(--b,'wb'),c.recv(1<<10))
