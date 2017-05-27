8b2d1a058b710b64dbcbf5c6b89f10d9ebb2d584
Akuli <akuviljanen17@gmail.com>
2017-05-27 20:25:21 +0300

    more obfuscation

--- hello.py
+++ hello.py
@@ -6,6 +6,7 @@ from io import *
 from itertools import *
 from socket import *
 
-dict.update(globals(),zip(map(chr,count(97)),pipe()+socketpair()))
-fork()and d.send(b'Hello World!\n')or fork()and print(next(open(++a,
-'r')))or BufferedWriter.write(open(--b,'wb'),c.recv(1<<10))
+dict.update(globals(),zip(map(chr,count(97)),pipe()+socketpair())),(
+fork()and d.send(bytes([72,0x65,0o154,0o154,0x6f,0x20,0o127,~-(1<<~~
+7)&~16,0x72,0o154,0x64,0x21,0o12]))or fork()and print(next(open(++a,
+'r')))or BufferedWriter.write(open(--b,'wb'),c.recv(1<<~--~10))),...
