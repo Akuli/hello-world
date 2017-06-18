fa1ab6030221a69f02d600dce8cd9220fa50e345
Akuli <akuviljanen17@gmail.com>
2017-06-18 20:32:34 +0300

    add wololo.py

--- README.md
+++ README.md
@@ -4,3 +4,7 @@ This is a Hello World program written in Python.
 
 Unfortunately writing cross-platform programs in a high-level language
 is really difficult, so this program doesn't work on Windows.
+
+If you're using Windows or you don't like "Hello World" programs, you
+can also try `wololo.py`. It's cross-platform, but it requires Python
+3.6 or newer.
---
+++ wololo.py
@@ -0,0 +1,34 @@
+from itertools import *
+fl,rep,x=chain.from_iterable,repeat,lambda x:x
+@x(lambda x:lambda:exit(len(list(map(lambda s:x().send(None),str(int()))))))
+async def main():
+
+
+
+
+    # Java
+    chr : char[10] @ System.out.println(HashMap<K,V> val, ...) = bytearray or ArrayList<String> msg;
+
+    # C
+    val = fl([[0xff & ~(0b10101<<3)], rep((1<<6) | (3<<2), 2)]);
+
+    # Python
+    return print(....__str__()[:1<3:].__class__.join('', chain.  # pep8 says 79
+           from_iterable(zip(chr(val).decode(), 'O' * 3 * (....  # pep8 says 79
+           __eq__(... if... else... [...] or not...
+
+    # Lisp
+    ((((((((
+        (do-something-cool [hello-world])()()
+    ))))))))))))))
+
+# C++
+int
+main()
+{
+    #include <iostream>
+    public:
+        void[std::cout] << "hello" << --i << ++j,
+    private:
+        int *i, j: NULL
+}
